4e8e2aa5c2b0e999b08a7e98dec3606149f00c4e
Santiago Campo <scampo@wolfram.com>
2023-11-26 01:32:05 -0500

    Added Utility functions for parsing actions

---
+++ python3/utilities.py
@@ -0,0 +1,42 @@
+from typing import Dict
+from typing import Union
+
+_move_set = set(("up", "down", "left", "right"))
+
+def parse_move(move: str, unit_id: str):
+    if move in _move_set:
+        packet = {"type": "move", "move": move, "unit_id": unit_id}
+        return packet
+
+def parse_bomb(unit_id: str):
+    packet = {"type": "bomb", "unit_id": unit_id}
+    return packet
+
+def parse_detonate(x, y, unit_id: str):
+    packet = {"type": "detonate", "coordinates": [
+        x, y], "unit_id": unit_id}
+    return packet
+
+# returns coordinates of the first bomb placed by a unit
+def _get_bomb_to_detonate(unit, raw_state: Dict) -> Union[int, int] or None:
+    entities = raw_state.get("entities")
+    bombs = list(filter(lambda entity: entity.get(
+        "unit_id") == unit and entity.get("type") == "b", entities))
+    bomb = next(iter(bombs or []), None)
+    if bomb != None:
+        return [bomb.get("x"), bomb.get("y")]
+    else:
+        return None
+
+def parse_action(action: str, unit_id: str, raw_state: Dict):
+    if action in ["up", "left", "right", "down"]:
+        return parse_move(action, unit_id)
+    elif action == "bomb":
+        return parse_bomb(unit_id)
+    elif action == "detonate":
+        bomb_coordinates = _get_bomb_to_detonate(unit_id, raw_state)
+        if bomb_coordinates != None:
+            x, y = bomb_coordinates
+            return parse_detonate(x, y, unit_id)
+    else:
+        print(f"Unhandled action: {action} for unit {unit_id}")
